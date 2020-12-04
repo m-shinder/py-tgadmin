2f9dd844546c48279972dc9e98938af25d90557e
Mihail <mihail@shinder.ml>
2020-12-04 23:16:17 +0200

    Add /ban

--- bot/command_handler.py
+++ bot/command_handler.py
@@ -1,5 +1,29 @@
+import telebot
 from bot import tgbot
+
+@tgbot.message_handler(commands=['ban'], func = lambda m: m.chat.type in ['group', 'supergroup' ] )
+def ban(msg):
+    if tgbot.get_chat_member(msg.chat.id, msg.from_user.id).status in ['creator', 'administrator']:
+        if msg.reply_to_message:
+            tgbot.kick_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, 0)
+            tgbot.send_message(msg.chat.id, "banned")
+        else:
+            tgbot.send_message(msg.chat.id, "reply to message")
+    else:
+        tgbot.send_message(msg.chat.id, "you are not admin")
+
+@tgbot.message_handler(commands=['mute'], func = lambda m: m.chat.type in ['group', 'supergroup' ])
+def mute(msg):
+    if tgbot.get_chat_member(msg.chat.id, msg.from_user.id).status in ['creator', 'administrator']:
+        if msg.reply_to_message:
+            permissions = telebot.telegram.ChatPermissions()
+#            tgbot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, )
+            tgbot.send_message(msg.chat.id, "banned")
+        else:
+            tgbot.send_message(msg.chat.id, "reply to message")
+    else:
+        tgbot.send_message(msg.chat.id, "you are not admin")
 
 @tgbot.message_handler()
 def echo(msg):
-    tgbot.send_message(msg.from_user.id, msg.text)
+    print(msg)
