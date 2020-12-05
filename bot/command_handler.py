38181ed32c3f7bc053b6f65af4378b8af1b2ea92
Mihail <mihail@shinder.ml>
2020-12-05 14:24:16 +0200

    add /mute command

--- bot/command_handler.py
+++ bot/command_handler.py
@@ -16,9 +16,9 @@ def ban(msg):
 def mute(msg):
     if tgbot.get_chat_member(msg.chat.id, msg.from_user.id).status in ['creator', 'administrator']:
         if msg.reply_to_message:
-            permissions = telebot.telegram.ChatPermissions()
-#            tgbot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, )
-            tgbot.send_message(msg.chat.id, "banned")
+            permissions = telebot.types.ChatPermissions(can_send_messages=False)
+            tgbot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, permissions)
+            tgbot.send_message(msg.chat.id, "muted")
         else:
             tgbot.send_message(msg.chat.id, "reply to message")
     else:
